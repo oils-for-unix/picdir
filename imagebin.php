b7697c899d941a6efcce6c71f12ec2c5e1b51bd2
Andy Chu <andy@oilshell.org>
2020-12-25 23:57:11 -0800

    Planning out the flow

--- imagebin.php
+++ imagebin.php
@@ -1,5 +1,7 @@
 <?php
 
+// NOTE: The web server has to be configured to serve out of both of these
+// dirs.
 $upload_dir = getenv('IMAGEBIN_UPLOAD_DIR');
 $cache_dir = getenv('IMAGEBIN_CACHE_DIR');
 
@@ -78,6 +80,8 @@ imagecopyresampled(
 
 // Catch the image data
 ob_start();
+
+// TODO: Use imagepng() and imagegif()
 imagejpeg($new, null, 90);
 $data = ob_get_clean();
 
---
+++ resize.php
@@ -0,0 +1,27 @@
+<?php
+
+// TODO:
+// 1. Check if the file exists in the data dir
+// 2. Check if the resized version already exists in the cache dir
+//    If not, create it.
+// 3. 301 Permanent Redirect to the cached version
+//    https://www.seoclarity.net/resources/knowledgebase/use-301-redirect-vs-302-redirect-15683/
+//
+// The web server has to be configured to serve it.
+
+echo '<pre>';
+
+$name = $_GET['name'];
+$width = $_GET['w'];
+$height = $_GET['h'];
+
+echo "name = " . $name . "\n";
+echo "width = " . $width . "\n";
+echo "height  = " . $height . "\n";
+
+echo '</pre>';
+
+?>
+
+
+
