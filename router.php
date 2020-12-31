d5160105a9e958407a06e91f3313d4d7c3dfd68d
Andy Chu <andy@oilshell.org>
2020-12-30 18:25:09 -0800

    Disable some logging.

--- router.php
+++ router.php
@@ -4,7 +4,7 @@
 // Get rid of .php extensions, so we can rewrite it in a different language.
 
 $uri = $_SERVER['REQUEST_URI'];
-error_log("uri = $uri");
+// error_log("uri = $uri");
 
 // get rid of query string
 $path = parse_url($uri, PHP_URL_PATH);
@@ -13,9 +13,9 @@ $path = parse_url($uri, PHP_URL_PATH);
 if (strpos($path, '/picdir') === 0) {
   $path = substr($path, strlen('/picdir'));
 }
-error_log("path = $path");
+// error_log("path = $path");
 
-// /upload, \/resize -> $1.php
+// /upload, /resize -> $1.php
 // We don't have to worry about index.php.
 $matches = array();
 if (preg_match('/^\/([a-z]+)$/', $path, $matches)) {
@@ -29,7 +29,11 @@ if (preg_match('/^\/([a-z]+)$/', $path, $matches)) {
 
 error_log("[router] Not handling '$path'");
 
-// upload.php, resize.php: NOT served
+// upload.php, resize.php: NOT handled here.  Dev servers falls back to its
+// normal logic.
+
+// TODO: This should serve a 404 under Apache.
+
 return false;
 
 ?>
